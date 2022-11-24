fc729d2484db912ee419c7a5fce74cb2b69990fb
sky19930112 <107167603+sky19930112@users.noreply.github.com>
2022-11-24 16:24:42 -0600

    work on though route

---
+++ routes/api/thoughtRoute.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const {
+  getThought,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction
+} = require("../../controllers/thoughtController.js");
+
+// /api/thoughts
+router.route("/").get(getThought).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
